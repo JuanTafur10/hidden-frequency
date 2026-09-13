23cf8fa834cbbfb056c7ea7ea1037cb785f21cb1
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2026-09-13 15:45:34 -0500

    Se termine el products.js

--- js/products.js
+++ js/products.js
@@ -5,6 +5,51 @@ const products = [
         category: "Vinilos",
         price: 250.000,
         description: "Edición especial 2LP del clasico de 2001. Estado: Mint (Muevo).",
-        image: assets/images/daft-punk-discovery.jfif
+        image: "assets/images/daft-punk-discovery.jpg"
+    },
+
+    {
+        id: 2,
+        title: "Boss DS-1 Distortion",
+        category: "Pedales",
+        price: 350.000,
+        description: "El pedal de distorsión más clásico. Tono agresivo para guitarra y sintes.",
+        image: "assets/images/boss-ds1.jpg"
+    },
+
+    {
+        id: 3,
+        title: "korg Minilogue XD",
+        category: "Insterumentos",
+        price: 980.000,
+        description: "Sintetizador analógico polifónico de 4 voces con efectos digitales.",
+        image: "assets/images/korg.jpg"
+    },
+
+    {
+        id: 4,
+        title: "MF DOOM - Madvillainy",
+        category: "Vinilos",
+        price: 42.00,
+        description: "Obra maestra del hip hop underground. Prensado de 180g.",
+        image: "assets/images/mf-doom-madvillainy.jpg"
+    },
+
+    {
+        id: 5,
+        title: "Strymon BigSky",
+        category: "Pedales",
+        price: 479.00,
+        description: "Reverb de reverberaciones algorítmicas de calidad de estudio.",
+        image: "assets/images/strymon-bigsky.jpg"
+    },
+
+    {
+        id: 6,
+        title: "Technics SL-1200MK7",
+        category: "Instrumentos",
+        price: 999.00,
+        description: "Tornamesa profesional para DJ de tracción directa. El estándar de la industria.",
+        image: "assets/images/technics-1200.jpg"
     }
 ]
