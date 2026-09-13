ad91fa658d98daf32c3b242e2930d1ecb6d712dd
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2026-09-13 15:31:26 -0500

    Se crea el primer producto y se conecta el .js con el html

---
+++ js/products.js
@@ -0,0 +1,10 @@
+const products = [
+    {
+        id: 1,
+        title: "Daft Punk - Discovery",
+        category: "Vinilos",
+        price: 250.000,
+        description: "Edición especial 2LP del clasico de 2001. Estado: Mint (Muevo).",
+        image: assets/images/daft-punk-discovery.jfif
+    }
+]
